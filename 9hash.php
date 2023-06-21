dc60a06755223cd7b19a1f1b97659eaddbb4a178
Phisarn Budwong <mythetooi@gmail.com>
2023-06-21 23:38:07 +0700

    Create 9hash.php

---
+++ 9hash.php
@@ -0,0 +1,34 @@
+<?php
+function 9hash($string) {
+    // ตรวจสอบว่าสตริงที่ให้มาไม่เป็นค่าว่าง
+    if (empty($string)) {
+        return false;
+    }
+
+    // กำหนดตัวแปรที่เก็บผลลัพธ์เริ่มต้น
+    $result = '';
+
+    // วนลูปตามจำนวนตัวอักษรในสตริง
+    for ($i = 0; $i < strlen($string); $i++) {
+        // หาค่า ASCII ของแต่ละตัวอักษร
+        $ascii = ord($string[$i]);
+
+        // ทำการเพิ่มค่า ASCII และนำมา modulo 26 เพื่อให้ได้ตัวอักษร A-Z
+        $newAscii = ($ascii + $i) % 26;
+
+        // แปลงค่า ASCII เป็นตัวอักษร
+        $newChar = chr($newAscii + 65);
+
+        // เก็บตัวอักษรใหม่ลงในผลลัพธ์
+        $result .= $newChar;
+    }
+
+    // คืนค่าผลลัพธ์ที่มีความยาว 9 ตัวอักษร
+    return substr($result, 0, 9);
+}
+
+$string = "Hello World";
+$hashedString = 9hash($string);
+echo "Original String: " . $string . "<br>";
+echo "Hashed String: " . $hashedString;
+?>
